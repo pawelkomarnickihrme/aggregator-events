bf0c447b98e9e0e1cebce5438ed95290eb988428
PawelKom <pawel.komarnicki@mikronika.com.pl>
2022-08-24 18:59:44 +0200

    working with header

---
+++ components/Footer.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+import styles from '../styles/Home.module.css'
+
+
+const Footer: NextPage = () => {
+    return (
+        <footer className={styles.footer}>
+            Powered by Komar
+        </footer>
+   
+    )
+  }
+export default Footer
---
+++ components/Header/Search.tsx
@@ -0,0 +1,6 @@
+const Search = () =>{
+    return(
+        <input type="text" placeholder="Szukaj..." name="q" ></input>
+    )
+}
+export default Search
