3574882309411bab5b9440fee34bd4898c59ec7c
PawelKom <pawel.komarnicki@mikronika.com.pl>
2022-09-24 13:30:51 +0200

    add styling to event

--- components/events/Event.tsx
+++ components/events/Event.tsx
@@ -1,18 +1,34 @@
 import Image from 'next/image'
-import style from './Events.module.css';
+import style from './Event.module.css';
 import {newEvent} from './Interface/newEvent'
+import { useState, useEffect } from 'react';
 
 
 
 const Event = ({title,date,photo,localization,content,drinks,side}:newEvent) => {
-  return <div className={style.event}>
-    {/* {<div><Image loader={() => photo} src={photo} height={339} width={356} alt={title}/></div>} */}
-    <h2>{title}</h2>
-    <div>{date}</div>
-    <div>{localization}</div>
-    <div>{content}</div>
-    <div>{drinks}</div>
-    <div>{side}</div>
-  </div>;
-};
+  const [domLoaded, setDomLoaded] = useState(false);
+
+  useEffect(() => {
+    setDomLoaded(true);
+  }, []);
+
+  return(
+    <>
+     {domLoaded && 
+      (<div className={style.event}>
+        <div className = {style.image}>
+          <Image loader={() => photo} src={photo} height={339} width={356} alt={title}/>
+        </div>
+        <div className={style.prev}>
+          <h2 className={style.title}>{title}</h2>
+          <div className={style.date}>{date}</div>
+          <div className={style.localization}>{localization}</div>
+          <div className={style.content}>{content}</div>
+          <div className={style.drinks}>{drinks}</div>
+          <div className={style.side}>{side}</div>
+        </div>
+      </div>)}
+  </>
+  )
+}
 export default Event;
